80481259e383824cf6460bf4e32857f3ce422daf
rs222kn <rs222kn@student.lnu.se>
2016-11-10 18:43:58 +0100

    GREEN

--- model/move.rb
+++ model/move.rb
@@ -3,19 +3,27 @@ class Move
 
   def initialize
     @input_directions = [:x, :y]
+    
   end
 
-  def get_position_object(x, y)
+  def get_position_object
 
   end
 
   def string_to_position_parser(string)
-    parts = string.split('')
+    
+    parts = string.split ''
     direction = parts[0].to_sym
+    steps = parts[1].to_i
+
     until @input_directions.include? direction 
       raise ArgumentError, 'Unknown direction'
     end
-     
+    
+    if steps >= 3 || steps < 0
+      raise ArgumentError, 'Invalid position'
+    end
+    
   end
   
 end
