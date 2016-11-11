0d4461bbc50317032820d4605cb2f278dfa3be65
rs222kn <rs222kn@student.lnu.se>
2016-11-11 17:06:52 +0100

    GREEN: is player winner does not have 3x in row

--- test/rulesTest.rb
+++ test/rulesTest.rb
@@ -32,9 +32,23 @@ describe 'Rules class' do
       player = mock()
       player.expects(:get_all_moves).returns(testCase[i])
       assert_equal rules.is_player_winner(player), true
-    end
-    
+    end    
   end
+
+  it 'is player winner: should be false if player does not have 3 x in a row' do
+      rules = Rules.new
+      testCase = [
+        [{x: 0, y: 0}, {x: 1, y: 0}, {x: 2, y: 1}],
+        [{x: 2, y: 0}, {x: 1, y: 2}, {x: 0, y: 0}],
+        [{x: 0, y: 1}, {x: 2, y: 0}, {x: 1, y: 0}]]
+      
+      3.times do |i| 
+        player = mock()
+        player.expects(:get_all_moves).returns(testCase[i])
+        assert_equal rules.is_player_winner(player), false
+      end    
+    end
+
 
   it 'is player winner: should be true if player has diagonal 3' do
     rules = Rules.new
