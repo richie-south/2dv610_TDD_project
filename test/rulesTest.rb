f4fb6ccbadaec6d3ba0ee6779b1eec2aa1b03d3f
rs222kn <rs222kn@student.lnu.se>
2016-11-11 16:03:48 +0100

    rewritten rules class tests

--- test/rulesTest.rb
+++ test/rulesTest.rb
@@ -7,11 +7,32 @@ require 'mocha/mini_test'
 require_relative '../model/rules'
 
 describe 'Rules class' do
-  it 'is player winner: should return true if player has 3 positions in a row' do
-    
+  it 'is player winner: should return true if player has 3 y in a row' do
     rules = Rules.new
-    player = mock()
-    player.expects(:get_all_moves).returns([{x: 0, y: 0}, {x: 0, y: 1}, {x: 0, y: 2}])
-    assert_equal rules.is_player_winner(player), true
+    testCase = [
+      [{x: 0, y: 0}, {x: 0, y: 1}, {x: 0, y: 2}],
+      [{x: 0, y: 2}, {x: 0, y: 1}, {x: 0, y: 0}],
+      [{x: 0, y: 0}, {x: 0, y: 2}, {x: 0, y: 1}]]
+
+    3.times do |i|
+      player = mock()
+      player.expects(:get_all_moves).returns(testCase[i])
+      assert_equal rules.is_player_winner(player), true
+    end
+  end
+
+  it 'is player winner: should be true if player has 3 x in a row' do
+    rules = Rules.new
+    testCase = [
+      [{x: 0, y: 0}, {x: 1, y: 0}, {x: 2, y: 0}],
+      [{x: 2, y: 0}, {x: 1, y: 0}, {x: 0, y: 0}],
+      [{x: 0, y: 0}, {x: 2, y: 0}, {x: 1, y: 0}]]
+    
+    3.times do |i| 
+      player = mock()
+      player.expects(:get_all_moves).returns(testCase[i])
+      assert_equal rules.is_player_winner(player), true
+    end
+    
   end
 end
