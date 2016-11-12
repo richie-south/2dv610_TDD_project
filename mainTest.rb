fd2401659e07ff7ed9c2faf7514b1952427efee0
rs222kn <rs222kn@student.lnu.se>
2016-11-12 18:41:22 +0100

    Moved test/main.rb to root, renamed test/main.rb to mainTest.rb

---
+++ mainTest.rb
@@ -0,0 +1,8 @@
+
+require 'simplecov'
+SimpleCov.start
+require_relative './test/playerTest'
+require_relative './test/moveTest'
+require_relative './test/rulesTest'
+require_relative './test/gameBordViewTest'
+require_relative './test/gameControllerTest'
--- test/main.rb
+++
@@ -1,7 +0,0 @@
-
-require 'simplecov'
-SimpleCov.start
-require_relative './playerTest'
-require_relative './moveTest'
-require_relative './rulesTest'
-require_relative './gameBordViewTest'
