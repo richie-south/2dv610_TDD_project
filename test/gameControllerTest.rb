0f731fe54d3e84cf3aa08286454d523d18c54060
rs222kn <rs222kn@student.lnu.se>
2016-11-12 16:02:15 +0100

    RED: get player name

--- test/gameControllerTest.rb
+++ test/gameControllerTest.rb
@@ -5,11 +5,23 @@ require 'minitest/spec'
 require 'minitest/unit'
 require 'mocha/mini_test'
 require_relative '../controller/gameController'
+require_relative '../model/player'
 
 describe 'GameController class' do
-  it 'get name inputs should' do
-    
-    
+  it 'get player name should return instance of player with name' do
+    name = 'Jonas'
+    r = mock()
+    m = mock()
+    g = mock()
+    c = mock()
+    inputOrder = sequence('inputOrder')
+    c.expects(:display_enter_player_name).in_sequence(inputOrder)
+    c.expects(:get_input).in_sequence(inputOrder).returns(name)
+
+    gameController = GameController.new(r, m, g, c)
+    actual = gameController.get_player_name
+    assert_instance_of Player, actual
+    assert_equal actual.get_name, name
   end
 
 end
