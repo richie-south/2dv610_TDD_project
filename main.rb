b93698df61c2f91b40ab62171e8d36012f25bacd
rs222kn <rs222kn@student.lnu.se>
2016-11-12 18:42:03 +0100

    do run!

--- main.rb
+++ main.rb
@@ -4,8 +4,10 @@ require_relative './view/gameBordView'
 require_relative './view/controllsView'
 require_relative './controller/gameController'
 
-GameController.new(
+gameController = GameController.new(
   Rules.new, 
   Move.new, 
   GameBordView.new,
   ControllsView.new)
+
+gameController.run
