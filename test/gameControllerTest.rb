09f41f1aec4efedd0f5b4ac3b75c9b6375ecb45d
rs222kn <rs222kn@student.lnu.se>
2016-11-12 17:37:07 +0100

    test refactor

--- test/gameControllerTest.rb
+++ test/gameControllerTest.rb
@@ -46,15 +46,19 @@ describe 'GameController class' do
     m = mock()
     g = mock()
     c = mock()
+    expected = {x: 1, y: 1}
     inputOrder = sequence('inputOrder')
     c.expects(:display_enter_first_coordinate).in_sequence(inputOrder)
     c.expects(:get_input).in_sequence(inputOrder).returns('x1')
+    m.expects(:string_to_position_parser).in_sequence(inputOrder).returns({x:1})
     c.expects(:display_enter_second_coordinate).in_sequence(inputOrder)
     c.expects(:get_input).in_sequence(inputOrder).returns('y1')
+    m.expects(:string_to_position_parser).in_sequence(inputOrder).returns({y:1})
+    m.expects(:merge_position_objects).in_sequence(inputOrder).returns(expected)
 
     gameController = GameController.new(r, m, g, c)
     actual = gameController.get_player_coordinate_inputs 'Jonas'
-    assert_equal({x: 1, y: 1}, actual)
+    assert_equal(expected, actual)
 
   end
 
