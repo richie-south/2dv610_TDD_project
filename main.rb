62ccd5e9e2c78d3bdff333f4e48047ee7fe2d6d4
rs222kn <rs222kn@student.lnu.se>
2016-11-12 15:18:34 +0100

    Updated main file

--- main.rb
+++ main.rb
@@ -1,3 +1,11 @@
-require 'simplecov'
-SimpleCov.start
-require_relative './test/playerTest'
+require_relative './model/rules'
+require_relative './model/move'
+require_relative './view/gameBordView'
+require_relative './view/controllsView'
+require_relative './controller/gameController'
+
+GameController.new(
+  Rules.new, 
+  Move.new, 
+  GameBordView.new,
+  ControllsView.new)
