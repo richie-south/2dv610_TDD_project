a7b702af591eb35629c90ef48df5579c7a6da042
rs222kn <rs222kn@student.lnu.se>
2016-11-12 17:28:02 +0100

    GREEN:

--- controller/gameController.rb
+++ controller/gameController.rb
@@ -9,6 +9,15 @@ class GameController
     @controllsView = controllsView
   end
 
+
+  def get_player_coordinate_inputs(name)
+    @controllsView.display_enter_first_coordinate
+    @controllsView.get_input
+    @controllsView.display_enter_second_coordinate
+    @controllsView.get_input
+    {x:1, y:1}
+  end
+
   def get_player_name
     @controllsView.display_enter_player_name
 
