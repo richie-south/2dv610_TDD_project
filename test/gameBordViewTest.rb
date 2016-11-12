4151f6af20d4925e20e35931cfeb38bc53b36cc4
rs222kn <rs222kn@student.lnu.se>
2016-11-12 15:17:22 +0100

    Added test files for gameBordView and gameController

---
+++ test/gameBordViewTest.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+gem 'mocha'
+require 'minitest/autorun'
+require 'minitest/spec'
+require 'minitest/unit'
+require 'mocha/mini_test'
+require_relative '../view/gameBordView'
+
+describe 'GameBordView class' do
+  it '' do
+    
+  end
+
+end
---
+++ test/gameControllerTest.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+gem 'mocha'
+require 'minitest/autorun'
+require 'minitest/spec'
+require 'minitest/unit'
+require 'mocha/mini_test'
+require_relative '../controller/gameController'
+
+describe 'GameController class' do
+  it 'get name inputs should' do
+    
+    
+  end
+
+end
