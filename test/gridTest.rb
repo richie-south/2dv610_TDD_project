df6c25f99050db9aaccb3c751d9893f805b8afe0
rs222kn <rs222kn@student.lnu.se>
2016-11-16 13:27:15 +0100

    RED: arrays should hold value false att every position

--- test/gridTest.rb
+++ test/gridTest.rb
@@ -16,11 +16,19 @@ describe 'Grid class' do
   it 'should return array with arrays in every position equal to the size of initial array' do
     size = 10
     grid = Grid.new size
-    
-    
     size.times do |i|
       assert_equal size, grid.get_grid[i].length
     end
-    
+  end
+
+  it 'should have value false in every position of arrays on array' do
+    size = 10
+    grid = Grid.new size
+    size.times do |i|
+      
+      grid.get_grid[i].each do |value|
+        assert_equal false, value
+      end
+    end
   end
 end
