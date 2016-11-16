dfe66ddf82753caa6d5c7415b332b3677b66d55d
rs222kn <rs222kn@student.lnu.se>
2016-11-16 13:23:22 +0100

    RED: should return array with arrays

--- test/gridTest.rb
+++ test/gridTest.rb
@@ -12,4 +12,15 @@ describe 'Grid class' do
     grid = Grid.new 3
     assert_equal 3, grid.get_grid.length 
   end
+
+  it 'should return array with arrays in every position equal to the size of initial array' do
+    size = 10
+    grid = Grid.new size
+    
+    
+    size.times do |i|
+      assert_equal size, grid.get_grid[i].length
+    end
+    
+  end
 end
